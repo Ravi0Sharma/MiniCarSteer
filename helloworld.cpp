3f383d98998bd3bd15324c651fa91f74aa4bf6b8
Emma Olmås <emmaolm@student.chalmers.se>
2025-04-02 13:49:18 +0000

    Revert "Add my own name"
    This reverts commit a3e15325527f6e7855f0fefc6a98105f866d9a8e

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,15 +2,13 @@
 #include <string>
 #include "PrimeChecker.hpp"
 
-int main(int argc, char **argv)
-{
-
-    if (argc == 2)
-    {
+int main (int argc, char** argv) {
+    
+    if (argc == 2) {
         int number = std::stoi(argv[1]);
         PrimeChecker pc;
 
-        std::cout << "Olmås, Emma; "
+        std::cout << "LAST NAME, FIRST NAME; "
                   << number << " is a prime number? "
                   << pc.isPrime(number)
                   << std::endl;
