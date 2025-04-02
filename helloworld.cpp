5c9e95059763fbd6f111e1c06246681d7ef56660
eriklidb <eriklidb@student.chalmers.se>
2025-04-02 15:47:32 +0200

    #3 - Add comments into the main file
    The comment explains how the execution of the main works

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,6 +4,8 @@
 
 int main (int argc, char** argv) {
     
+    /* This statement checks whether exactly one argument is provided via the command line. */
+    /* If an argument is provided, the program will proceed to check if the number is a prime. */
     if (argc == 2) {
         int number = std::stoi(argv[1]);
         PrimeChecker pc;
